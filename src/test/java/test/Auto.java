edade4a10005a759ae0853e9ad25da6cabdd3953
Julián E. Londoño <julondonor@unal.edu.co>
2022-03-21 22:56:46 -0500

    Update Auto.java
    Second attempt

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -3,20 +3,27 @@ package test;
 public class Auto {
 	public String modelo;
 	public int precio;
-	public Asiento[] asientos;
+	public Asiento[] asientos = new Asiento[10];
 	public String marca;
 	public Motor motor;
 	public int registro;
 	public static int cantidadCreados;
 	
 	public int cantidadAsientos() {
-		return asientos.length;
+		int count = 0;
+		for(Asiento asiento: asientos) {
+			if (asiento instanceof Asiento) {
+				count++;
+			}
+		}
+		return count;
 	}
 	public String verificarIntegridad(){
 		for (Asiento asiento : asientos) {
+			if(asiento instanceof Asiento) {
 			if(asiento.registro != this.registro) {
 				return "Las piezas no son originales";
-			}
+			}}
 		}
 		if(motor.registro != this.registro) {
 			return "Las piezas no son originales";
